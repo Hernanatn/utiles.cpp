8fbf4254ada5a099e80b0f51500968528e0c60b3
Herni <hernanatn@gmail.com>
2025-01-19 14:18:23 -0300

    .

---
+++ fuente/algebra/vector.hpp
@@ -0,0 +1,16 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+namespace utiles {
+namespace algebra {
+namespace vectores {
+
+
+
+
+
+}
+}
+}
+
+#endif
--- fuente/tdas/lista.hpp
+++ fuente/tdas/lista.hpp
@@ -1,13 +1,15 @@
 #ifndef LISTA_ENLAZADA_H
 #define LISTA_ENLAZADA_H
 
+namespace utiles {
+
 namespace tda {
 
 
 
 
 
-
+}
 }
 
 #endif
